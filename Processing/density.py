17f42b6dd18cfc76d07f3c28d0254953b9802374
emlorraine <52936908+emlorraine@users.noreply.github.com>
2021-10-21 15:46:46 -0500

    entropy results

---
+++ Processing/density.py
@@ -0,0 +1,9 @@
+from skimage.measure.entropy import shannon_entropy
+from skimage import io
+
+simple_img = io.imread("simple_tree.png") 
+print(shannon_entropy(simple_img[:,:,0]))
+
+complex_img = io.imread("tree.png") 
+print(shannon_entropy(complex_img[:,:,0]))
+
