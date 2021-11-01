056c8d2200b20153f0793ea8fdb21ad697a31fec
emlorraine <52936908+emlorraine@users.noreply.github.com>
2021-11-01 12:10:40 -0500

    training model correctly

--- Training/training.py
+++ Training/training.py
@@ -1,48 +1,29 @@
 import matplotlib.pyplot as plt
-import seaborn as sns
-
-import keras
-from keras.models import Sequential
-from keras.layers import Dense, Conv2D , MaxPool2D , Flatten , Dropout 
-from keras.preprocessing.image import ImageDataGenerator
-from keras.optimizers import adam_v2
-
-from sklearn.metrics import classification_report,confusion_matrix
-
+import numpy as np
+import os
+import PIL
 import tensorflow as tf
 
-import cv2
-import os
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
 
-import numpy as np
+import pathlib
 
-labels = ['bar chart', 'line chart', 'pie chart','scatter plot']
-img_size = 224
+IMAGE_SHAPE = (224, 224)
+TRAINING_DATA_DIR = 'data/training/'
+VALID_DATA_DIR = 'data/validation/'
 
-def get_data(data_dir):
-    data = [] 
-    for label in labels: 
-        path = os.path.join(data_dir, label)
-        print(path)
-        class_num = labels.index(label)
-        for img in os.listdir(path):
-            try:
-                img_arr = cv2.imread(os.path.join(path, img))[...,::-1] #convert BGR to RGB format
-                resized_arr = cv2.resize(img_arr, (img_size, img_size)) # Reshaping images to preferred size
-                data.append([resized_arr, class_num])
-            except Exception as e:
-                print(e)
-    return np.array(data)
-
-train = get_data('./data/all')
-
-#     if(i[1] == b):
-#         l.append("bar_chart")
-#     elif(i[1] == s):
-#         l.append("scatter_plot")
-#     elif(i[1] == p):
-#         l.append("pie_chart")
-#     elif(i[1] == l):
-#         l.append("line_graph")
-# sns.set_style('darkgrid')
-# sns.countplot(l)
+datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+    rescale=1./255
+)
+train_generator = datagen.flow_from_directory(
+    TRAINING_DATA_DIR,
+    shuffle=True,
+    target_size=IMAGE_SHAPE,
+)
+valid_generator = datagen.flow_from_directory(
+    VALID_DATA_DIR,
+    shuffle=False,
+    target_size=IMAGE_SHAPE,
+)
