8232a7d4ea29b112814748ea4eb090361aa2faae
emlorraine <52936908+emlorraine@users.noreply.github.com>
2021-12-07 11:55:32 -0600

    scraping reddit

--- scrapers/reddit.py
+++ scrapers/reddit.py
@@ -3,6 +3,28 @@ import pandas as pd
 import sys
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+from datetime import datetime
+
+subreddits = [
+    "worldnews",
+    "news",
+    "worldpolitics",
+    "worldevents",
+    "business",
+    "Economics",
+    "politics",
+    "uspolitics",
+    "AmericanPolitics",
+    "AmericanGovernment",
+    "Libertarian",
+    "socialism",
+    "progressive",
+    "conservative",
+    "democrats",
+    "Liberal",
+    "Republican",
+    "LibertarianLeft"
+]
 
 def update(df):
     scope = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/spreadsheets","https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
@@ -14,7 +36,7 @@ def update(df):
 
 
 def scrape():
-    posts = []
+    posts_dict = {"Date": [], "Subreddit": [], "Title": [], "Post Text": [], "ID": [], "Score": [], "Total Comments": [], "Post URL": []}
 
     id = "ROpOaQiBgUAsLw"
     key = "xQkoVMppm1pDAFJrNo2Dgr33ItW1yA"
@@ -22,14 +44,19 @@ def scrape():
 
     reddit = praw.Reddit(client_id=id, client_secret=key, user_agent=agent)
 
-    for submission in reddit.subreddit('all').hot(limit=100000):
-        url = submission.url
-        post = submission.selftext
-        if ((" graph " in post or "viz" in post) and (url.endswith(('.jpg', '.png', '.gif', '.jpeg')))):
-            print(url)
-        # if url.endswith(('.jpg', '.png', '.gif', '.jpeg')):
-        #     posts.append([str(submission.title), str(submission.score), str(submission.id), str(submission.subreddit), str(submission.url), str(submission.num_comments), str(submission.selftext), str(submission.created),])
+    for subreddit in subreddits:
+        for submission in reddit.subreddit(subreddit).hot(limit=100):
+            posts_dict["Date"].append(datetime.today().strftime('%Y-%m-%d'))
+            posts_dict["Subreddit"].append(subreddit)
+            posts_dict["Title"].append(submission.title)
+            posts_dict["Post Text"].append(submission.selftext)
+            posts_dict["ID"].append(submission.id)
+            posts_dict["Score"].append(submission.score)
+            posts_dict["Total Comments"].append(submission.num_comments)
+            posts_dict["Post URL"].append(submission.url)
     
+    posts = pd.DataFrame(posts_dict)
+    print(posts)
     # posts = pd.DataFrame(posts,columns=['title', 'score', 'id', 'subreddit', 'url', 'num_comments', 'body', 'created'])
     # update(posts); 
 
