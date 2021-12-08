283113d300303150727c28335f2ade85bfffa8d8
emlorraine <52936908+emlorraine@users.noreply.github.com>
2021-12-08 11:36:26 -0600

    finished reddit woohoo

--- scrapers/reddit.py
+++ scrapers/reddit.py
@@ -2,16 +2,21 @@ import praw
 import pandas as pd
 import sys
 import gspread
+from gspread_dataframe import get_as_dataframe, set_with_dataframe
+
 from oauth2client.service_account import ServiceAccountCredentials
+from google.oauth2.service_account import Credentials
+
 from datetime import datetime
+import json
+import os 
+
 
 subreddits = [
     "worldnews",
     "news",
     "worldpolitics",
     "worldevents",
-    "business",
-    "Economics",
     "politics",
     "uspolitics",
     "AmericanPolitics",
@@ -26,18 +31,8 @@ subreddits = [
     "LibertarianLeft"
 ]
 
-def update(df):
-    scope = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/spreadsheets","https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_name("service_account.json", scope)
-    client = gspread.authorize(creds)
-    sheet = client.open("reddit").sheet1 
-    data = sheet.get_all_records()  
-    sheet.update([df.columns.values.tolist()] + df.values.tolist())
-
-
 def scrape():
     posts_dict = {"Date": [], "Subreddit": [], "Title": [], "Post Text": [], "ID": [], "Score": [], "Total Comments": [], "Post URL": []}
-
     id = "ROpOaQiBgUAsLw"
     key = "xQkoVMppm1pDAFJrNo2Dgr33ItW1yA"
     agent = "viz_scraper"
@@ -46,18 +41,46 @@ def scrape():
 
     for subreddit in subreddits:
         for submission in reddit.subreddit(subreddit).hot(limit=100):
-            posts_dict["Date"].append(datetime.today().strftime('%Y-%m-%d'))
-            posts_dict["Subreddit"].append(subreddit)
-            posts_dict["Title"].append(submission.title)
-            posts_dict["Post Text"].append(submission.selftext)
-            posts_dict["ID"].append(submission.id)
-            posts_dict["Score"].append(submission.score)
-            posts_dict["Total Comments"].append(submission.num_comments)
-            posts_dict["Post URL"].append(submission.url)
+            if(submission.url):
+                posts_dict["Date"].append(datetime.today().strftime('%Y-%m-%d'))
+                posts_dict["Subreddit"].append(subreddit)
+                post_title = submission.title[0:500]
+                if(len(post_title)>1000):
+                    print(post_title)
+                
+                posts_dict["Title"].append(post_title)
+                post_text = submission.selftext[0:500]
+                if(len(post_text)>1000):
+                    print(post_text)
+                posts_dict["Post Text"].append(post_text)
+                posts_dict["ID"].append(submission.id)
+                posts_dict["Score"].append(submission.score)
+                posts_dict["Total Comments"].append(submission.num_comments)
+                posts_dict["Post URL"].append(submission.url)
+                
     
     posts = pd.DataFrame(posts_dict)
-    print(posts)
-    # posts = pd.DataFrame(posts,columns=['title', 'score', 'id', 'subreddit', 'url', 'num_comments', 'body', 'created'])
-    # update(posts); 
+    update(posts); 
+
+def update(df):
+    credentials = {
+        "type": "service_account",
+        "project_id": "reddit-334418",
+        "private_key_id": "dbc7344c3125d43531babfcee6706a230f58f675",
+        "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQClgTpRDRNajJuJ\nsdw+QtludCube8RbOpc4Ea+w/w+rG+ll6dOnnG67b9mI6NWwty8GtsA1GZ0xIdJ+\nh+6NPqL5ZPp92J/eQnhIZNLC0r3QZxRs5eNRxkJnAzTHo085MAMWZbB0Vdwm4O5n\nebDYnV+gliktgm3vrhgwafC3ecR2NgwonuQ/Ib1DYNjpR/OTG8vD0fu2zFoFZRU/\nqRy1rsKwizzDJgh/ifQ9yRMZ90mOos9CMrcT+TSCi43o3Hqj/L7Bnsv5XZ3qi/sg\nPurakZJMQbsMZOUl0NqkMKONIZ5qY37Q896FtvqKJl1df1ntJMI31siyAGFfxaAa\nWlBk3JmZAgMBAAECggEABOACT+JNJOzzEscakB7uizulEAtBG49liuf5UZU4n6Tz\nQlQ+LsooHNhxTFvQh5FSWAFIpGMzMJJNeR88MJ7mIlUOHrFsrBl42LEOcoXOVmOZ\nw/5DPuSt/FQ7tHra2dVipC6txcZW0MSkztBUZV0J66IFQw/FL80tbYMTpD9UHJD0\nRv3Ktv5jx1FtaaZkME/zJnyejgCQ/rerVcGs1pjjU8zdMhtSvTa1s9sYAvlzu/8k\n1pefkLsZuVP/pO4GrVN7d5v3TjF8nv6Y7Ira//5h1ke7a9VaZjQwGskQbT2wji30\nxyTrw3B1WieHbnkgyAkZg9m63aDijGF4dVi+qIRrjQKBgQDY593pE8SSCLRYQ1gG\nKtU8+un+9bz/NuTJXBKLafzP2s0J43ZfSjn0QNxh6FBeVb+XaaxXVM6wh+NXAsh/\nyUolU0YIzsuRkIZIksw9Z9IyzKWzNvIVcQHYu6KH7fmwhKFMhotVEpqWoXD8cQvP\nI2KG6b7j5iCOkIU/HAhHQVUCBwKBgQDDVbKQtaWC1JdggffeHs7+PEqIQaQDqUMx\nWQtIasgkkXqMM7aFPJ9Z/sBXWVsCER6rbLTEhgkEjpOMpuwT3xc0Sr+8/PXRGsF9\nA+G3ZTB1I+X+E/ofH9z3eTHRqLBNKzyfdK0SfGIibjpK92coC6UxMfdAcPkD9G/9\nRhM2394fXwKBgDzxuSo6Aas+gt2h3mOtOUju/zxB856J3/KryhId74i/Y4j5vlK7\n2ljEuKdRzPMUiMaUTHYlQAXdyIS0JX2yIwElyrHC2PPHddOCW5yNRUQ8t/oI4DAi\nFnC9F8e1l8h/G4sS6qc2mPTl24cyhCzpNk/N8XK7QD6OYMIAsFrFAouVAoGBAINZ\nTAK88rfgBo6xtqBZLS2OEzw+j3Ca0AEN9GVU0JKudK50U6aSVkEo6eOSxXzFUE9L\ngN6plsTGrvckg5j1KeBS503I9+8NQ9Cx3IT6+TO72PsaKdXmEisjBtoJycuKaHB8\n/6hvlXm7j107sdUex40mITHnBbugEfJIvcDnlrCXAoGAcJmYYZB3IMn9/yEduBeu\nBIWM9IgOLBf/PIle15QSZmydPmTZAAFCHSa828qhrrvXPd/r1V2cJKmyihuNylCR\n1HBhn56BdMqvqXPfBFArL4fsgttEejBsZGD0bvsnLdBPiLg8SN9BD4w8qVVs+EpO\n1CvQnHOeuo6UctJX/snnRc0=\n-----END PRIVATE KEY-----\n",
+        "client_email": "reddit@reddit-334418.iam.gserviceaccount.com",
+        "client_id": "106682552318210817559",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/reddit%40reddit-334418.iam.gserviceaccount.com"
+    } 
+    gc = gspread.service_account_from_dict(credentials)
+    sh = gc.open("reddit")
+    date_string = datetime.now().strftime("%m/%d/%y")
+    worksheet = sh.add_worksheet(title=date_string, rows="1700", cols="15")
+    set_with_dataframe(worksheet, df)
 
 scrape()
+
+    
