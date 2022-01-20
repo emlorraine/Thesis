3c7c7d7d4696fc910b46588770beb84dd0c6d993
emlorraine <52936908+emlorraine@users.noreply.github.com>
2022-01-20 14:10:51 -0600

    integrating soup strainer into scrapy-splash

--- Identify/urls/urls/spiders/urlspider.py
+++ Identify/urls/urls/spiders/urlspider.py
@@ -1,6 +1,9 @@
 import scrapy 
 from scrapy_splash import SplashRequest
 
+from bs4 import BeautifulSoup
+from bs4 import SoupStrainer
+
 
 class Url(scrapy.Spider):
     name = 'url'
@@ -8,9 +11,11 @@ class Url(scrapy.Spider):
         url = "https://www.cnn.com/interactive/2019/business/us-minimum-wage-by-year/index.html"
         yield SplashRequest(url=url, callback=self.parse)
     def parse(self, response):
-        html = response.body
-        for prop in html:
-            yield {
-                'html':prop
-            }
-        return html
+        custom_strainer = SoupStrainer(["svg","g"])
+        page_soup = BeautifulSoup(response.text, 'html.parser', parse_only=custom_strainer)
+        # soup = BeautifulSoup(response.text, 'lxml')
+        print(page_soup)
+        # html = response.body
+        # yield {
+        #     "url": response.url
+        # }
